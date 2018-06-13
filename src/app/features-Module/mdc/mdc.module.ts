c3c599ba70b8bd45d0775c2f9fd86d303ef86131
John Froi Dejaresco <johnfroi46@gmail.com>
2018-06-13 11:01:16 +0800

    : Initial commit  Changes to be committed:

---
+++ src/app/features-Module/mdc/mdc.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MdcFabModule,
+  MdcMenuModule
+} from '@angular-mdc/web';
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class MdcModule { }
