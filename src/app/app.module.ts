849838525f23fa40adcfe5a754f1b46f723a5bed
John Froi Dejaresco <johnfroi46@gmail.com>
2018-06-13 11:46:28 +0800

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core'; 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/features-Module/hero/hero.module.ts
+++ src/app/features-Module/hero/hero.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MdcFabModule,
-  MdcMenuModule
-} from '@angular-mdc/web';
+
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: []
 })
-export class MdcModule { }
+export class HeroModule { }
